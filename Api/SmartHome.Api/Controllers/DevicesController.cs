2d356a2a6499c2bf2c61f204f1a8d7731ffe1be0
lasse <lasse795@gmail.com>
2025-08-24 01:19:29 +0200

    DeviceController Init

---
+++ Api/SmartHome.Api/Controllers/DevicesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartHome.Slices.Devices.EntryPoints;
+using SmartHome.Shared;
+
+namespace SmartHome.Api;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DevicesController : ControllerBase {
+    private readonly IDevicesEntryPoint _entryPoint;
+
+    public DevicesController(IDevicesEntryPoint entryPoint) {
+        _entryPoint = entryPoint;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<Device>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAllDevices() {
+        var devices = await _entryPoint.GetDevicesAsync();
+        return Ok(devices);
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(typeof(Device), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetDeviceById([FromRoute]int id) {
+        var device = await _entryPoint.GetDeviceByIdAsync(id);
+        if(device == null) {
+            return NotFound();
+        }
+        return Ok(device);
+    }
+
+    [HttpGet("search")]
+    [ProducesResponseType(typeof(Device), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> SearchDeviceByName([FromRoute] string search) {
+        var device = await _entryPoint.SearchDeviceByNameAsync(search);
+        if(device == null) {
+            return NotFound();
+        }
+        return Ok(device);
+    }
+}
---
+++ Shared/SmartHome.Shared/Models/Device.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartHome.Shared {
+    public class Device {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public string IpAddress { get; set; } = string.Empty;
+        public bool Active { get; set; }
+    }
+}
